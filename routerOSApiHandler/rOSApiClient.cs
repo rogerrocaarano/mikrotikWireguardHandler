f14d66e6237bc69ad0aecfdd1a8358612fafce0c
Róger Roca Arano <rogerroca@reinseg.com>
2023-03-22 19:19:07 -0400

    Added Wireguard interfaces and peers handlers.

--- routerOSApiHandler/rOSApiClient.cs
+++ routerOSApiHandler/rOSApiClient.cs
@@ -1,18 +1,30 @@
 ﻿namespace routerOSApiHandler;
 
+/// <summary>
+/// Defines an object than handles RouterOS RestAPI calls.
+/// </summary>
 public class rOSApiClient
 {
-    protected string restApiServer { get; set; }
-    protected string restApiUser { get; set; }
-    protected string restApiPassword { get; set; }
-    protected bool useSSL { get; set; }
-    
+    // Mikrotik RestAPI Server FQDN
+    private string restApiServer;
+    // URI scheme
+    private bool useSSL;
+    // Authentication header for basic HTTP authentication
+    private System.Net.Http.Headers.AuthenticationHeaderValue AuthHeader;
+
+    /// <summary>
+    /// Creates an instance of rosApiClient than uses SSL by default.
+    /// </summary>
+    /// <param name="serverFqdn">Mikrotik RestAPI Server FQDN</param>
+    /// <param name="user">Router's username for RestAPI requests</param>
+    /// <param name="password">Router's password for RestAPI requests</param>
     public rOSApiClient(string serverFqdn, string user, string password)
     {
         restApiServer = serverFqdn;
-        restApiUser = user;
-        restApiPassword = password;
         useSSL = true;
+        // generate an authToken for basic HTTP Auth and create a header for the requests.
+        var authToken = System.Text.Encoding.UTF8.GetBytes($"{user}:{password}");
+        AuthHeader = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
     }
     /// <summary>
     /// Takes a single parameter named "request" and returns a string representing the URI for the RESTful API call.
@@ -25,21 +37,20 @@ public class rOSApiClient
     }
     /// <summary>
     /// takes a single parameter named "request" and returns a string representing the response from the RouterOS API.
-    /// The "request" parameter is used to construct the URI for the RESTful API call by calling the private "RequestUri" method.
+    /// The "request" parameter is used to construct the URI for the RESTful API call by calling the private
+    /// "RequestUri" method.
     /// </summary>
     public async Task<string> Get(string request)
     {
         // get the full URI for the request:
         var uri = RequestUri(request);
-        // create a new instance of the HttpClient and set the Authorization header of the HTTP request to
-        // the value "Basic" followed by the Base64-encoded authentication token. This authenticates the request
-        // with the RouterOS API using the username and password provided in the constructor.
         using var apiClient = new HttpClient();
-        var authToken = System.Text.Encoding.UTF8.GetBytes($"{restApiUser}:{restApiPassword}");  
-        apiClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
+        apiClient.DefaultRequestHeaders.Authorization = AuthHeader;
         // Send the RESTful API request to the RouterOS server.
         // The response from the RouterOS API is returned as a string.
-        var test = await apiClient.GetStringAsync(uri);
-        return test;
+        var response = await apiClient.GetStringAsync(uri);
+        return response;
     }
+    
+    
 }
