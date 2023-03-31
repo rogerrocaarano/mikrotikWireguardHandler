4015d869defef964b95bcfb9b355420403762f69
Róger Roca Arano <rogerroca@reinseg.com>
2023-03-31 19:20:04 -0400

    Created helpers functions to verify if an interface exists and to serialize WireguardInterface objects.
    Created SetInterface and DeleteInterface Methods.

--- routerOSApiHandler/Handlers/WireguardServer.cs
+++ routerOSApiHandler/Handlers/WireguardServer.cs
@@ -45,25 +45,69 @@ public class WireguardServer
     }
 
     /// <summary>
+    /// Helper function for finding if an interface exists in the Interfaces list.
+    /// </summary>
+    private bool InterfaceExists(string name)
+    {
+        return Interfaces.Exists(existingIface => name == existingIface.Name);
+    }
+    
+    /// <summary>
+    /// Helper function for serialize a WireguardInterface object removing null values in attributes.
+    /// </summary>
+    private string SerializeInterface(WireguardInterface iface)
+    {
+        var json = JsonConvert.SerializeObject(
+            iface,
+            new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore }
+        );
+        return json;
+    }
+    /// <summary>
     /// Creates a new Interface on the server.
     /// </summary>
     /// <param name="iface">WiregaurdInterface object containing the data to create a new interface.</param>
     public async Task NewInterface(WireguardInterface iface)
     {
         // Verify if the interface name already exists.
-        if (Interfaces.Exists(existingIface => iface.Name == existingIface.Name))
+        if (InterfaceExists(iface.Name))
         {
             var message = "Interface" + iface.Name + "already exists.";
             Console.WriteLine(message);
             return;
         }
         // Serialize the iface data, ignoring null parameters.
-        var json = JsonConvert.SerializeObject(
-            iface, 
-            new JsonSerializerSettings {NullValueHandling = NullValueHandling.Ignore}
-        ); 
+        var json = SerializeInterface(iface);
         // Send the request to the restAPI and update the Interfaces list.
         await _server.Put("interface/wireguard", json);
         await UpdateInterfaces();
+    }
+
+    /// <summary>
+    /// Updates existing interface parameters.
+    /// </summary>
+    /// <param name="iface">The interface object with updated parameters.</param>
+    public async Task SetInterface(WireguardInterface iface)
+    {
+        if (InterfaceExists(iface.Name))
+        {
+            var json = SerializeInterface(iface);
+            var requestPath = "interface/wireguard/" + iface.Name;
+            await _server.Patch(requestPath, json);
+            await UpdateInterfaces();
+        }
+    }
+
+    /// <summary>
+    /// Deletes an existing interface.
+    /// </summary>
+    /// <param name="iface">The interface object to delete.</param>
+    public async Task DeleteInterface(WireguardInterface iface)
+    {
+        if (InterfaceExists(iface.Name))
+        {
+            var requestPath = "interface/wireguard/" + iface.Name;
+            await _server.Delete(requestPath);
+        }
     }
 }
